a8b6cf12bf2a6289de302c14e6ee183040dca3db
Fr1xee <73335583+Fr1xee@users.noreply.github.com>
2022-03-26 23:42:28 +0700

    Revert "nenene"

--- src/main/java/com/example/graduationProjectServer/controller/LoginSignupController.java
+++ src/main/java/com/example/graduationProjectServer/controller/LoginSignupController.java
@@ -30,9 +30,8 @@ public class LoginSignupController {
             return ResponseEntity.badRequest().body(e);
         }
     }
-// endpoint login передаем емаил пароль
+
     @GetMapping("/login/{email}/{password}")
-    // pathvariable позволяет передать значения из урл выше 
     public ResponseEntity<String> authorizationUser(@PathVariable(value = "email") String email,
                                                     @PathVariable(value = "password") String password) {
         try {
@@ -41,18 +40,15 @@ public class LoginSignupController {
             } else if (password.equals("")) {
                 return new ResponseEntity<>("Введите пароль", HttpStatus.NO_CONTENT);
             } else {
-                // gettinguser изначально пустой, потом мы кладем внего все данные пользователя
                 UserStructure gettingUser = userRepo.findByEmail(email);
                 System.out.println(email + " " + password + " " + gettingUser);
                 if (gettingUser != null) {
-                    // проверка на правильность введенного пароля
                     if (password.equals(gettingUser.getPassword())) {
                         System.out.println("Авторизация что-то");
                         return ResponseEntity.ok("Авторизация прошла успешно");
                     } else {
                         return ResponseEntity.badRequest().body("Введённый пароль неверный");
                     }
-                    // если gettinguser = null то выводит это
                 } else {
                     return ResponseEntity.badRequest().body("Пользователя с таким email не существует");
                 }
